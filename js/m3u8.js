400bb48227c87c85514854d8a8e2869874906c24
O2bmm <xifangczy@gmail.com>
2022-05-22 03:13:54 +0800

    update m3u8

--- js/m3u8.js
+++ js/m3u8.js
@@ -1,145 +1,162 @@
-//正则
-var EXTINF = new RegExp("#EXTINF:(.*),");
+//获取m3u8_url
+var m3u8_url = new RegExp("[?]m3u8_url=([^\n]*)").exec(window.location.href)[1];
 
-//获取m3u8_url参数
-var results = new RegExp("[?]m3u8_url=([^\n]*)").exec(window.location.href);
-var url = results[1];
-
-//获取m3u8的参数
-results = new RegExp("\\.m3u8\\?([^\n]*)").exec(url);
-if (results) {
-	var m3u8_arg = results[1];
+//获取m3u8参数
+var m3u8_arg = new RegExp("\\.m3u8\\?([^\n]*)").exec(m3u8_url);
+if (m3u8_arg) {
+    m3u8_arg = m3u8_arg[1];
 }
-$("#m3u8_url").attr("href", url).html(url)
 
+$("#m3u8_url").attr("href", m3u8_url).html(m3u8_url)
+
+var BasePath;
+var RootPath;
+var m3u8_content;
 //获取url内容
-var html = $.ajax({ url: url, async: false }).responseText;
-html = html.split("\n");
+$.ajax({
+    url: m3u8_url, async: true, success: function (result) {
+        $("#m3u8").show();
+        BasePath = getManifestUrlBase();
+        RootPath = getManifestUrlRoot();
+        m3u8_content = result;
+        show_list();
+    }, error: function () {
+        $("#loading").show();
+        $("#m3u8").hide();
+        $("#loading .optionBox").html("获取m3u8内容失败");
+    }
+});
 
 //基本文件目录
-function getManifestUrlBase(URL_flag) {
-	let url_decode = URL_flag ? decodeURIComponent(url) : url;
-	url_decode = url_decode.split("?")[0];
-	let parts = url_decode.split("/");
-	parts.pop();
-	return parts.join("/") + "/";
+function getManifestUrlBase(decode = true) {
+    let url_decode = decode ? decodeURIComponent(m3u8_url) : m3u8_url;
+    url_decode = url_decode.split("?")[0];
+    let parts = url_decode.split("/");
+    parts.pop();
+    return parts.join("/") + "/";
 }
+
 //根目录
 function getManifestUrlRoot() {
-	var Path = url.split("/");
-	return Path[0] + "//" + Path[2];
-};
-var BasePath = getManifestUrlBase(true);
-var RootPath = getManifestUrlRoot();
-
-//验证是否远程文件
-function isRelative(url) {
-	var r = new RegExp("^(?:[a-z]+:)?//", "i");
-	return !r.test(url);
+    let Path = m3u8_url.split("/");
+    return Path[0] + "//" + Path[2];
 }
 
-//写入textarea
-function add_textarea(str) {
-	//ts文件加参数
-	results = new RegExp("[?]([^\n]*)").exec(str);
-	if (!results && m3u8_arg) {
-		str = str + "?" + m3u8_arg;
-	}
-	link = $("#media_file").val() + str + "\n";
-	$("#media_file").val(link);
+//修复url路劲
+function fixUrl(url) {
+    if (/^http[s]?:\/\/.+/i.test(url)) {
+        return url;
+    }
+    if (url[0] == "/") {
+        return RootPath + url;
+    }
+    return BasePath + url;
+}
+function GetFile(str) {
+    str = str.split("?")[0];
+    return str.split("/").pop();
 }
 
-function add_next_m3u8(link) {
-	$("#next_m3u8").append(
-		'<p><a href="/m3u8.html?m3u8_url=' + link + '">' + link + "</a></p>"
-	);
+function show_list(format = "") {
+    let count = 0;
+    let KeyURL = "";
+    let KeyIV = "";
+    let textarea = "";
+    $("#media_file").val("");
+    let m3u8_split = m3u8_content.split("\n");
+    for (let line of m3u8_split) {
+        if (line == "\n" || line == "\r" || line == "" || line == " ") {
+            continue;
+        }
+        //存在密钥
+        if (line.includes("URI=")) {
+            KeyURL = /URI="(.*)"/.exec(line)[1];
+            KeyURL = fixUrl(KeyURL);
+        }
+        if (line.includes("IV=")) {
+            KeyIV = /IV="(.*)"/.exec(line)[1];
+            KeyIV = fixUrl(KeyIV);
+        }
+        //ts文件
+        if (!line.includes("#")) {
+            count++;
+            line = fixUrl(line);
+            //判断是否m3u8
+            if (line.includes(".m3u8")) {
+                $("#m3u8").hide();
+                $("button").hide();
+                $("#more_m3u8").show();
+                $("#next_m3u8").append(
+                    '<p><a href="/m3u8.html?m3u8_url=' + line + '">' + GetFile(line) + "</a></p>"
+                );
+                continue;
+            }
+            //格式化
+            line = line.replace("\n", "").replace("\r", "");
+            if (format != "") {
+                line = format.replace("$url$", line);
+            }
+            let results = new RegExp("[?]([^\n]*)").exec(line);
+            if (!results && m3u8_arg) {
+                line = line + "?" + m3u8_arg;
+            }
+            textarea = textarea + line + "\n";
+        }
+    }
+    $("#media_file").val(textarea);
+
+    $("#count").html("共" + count + "个文件");
+    if (KeyURL !== "") {
+        $(".keyUrl").show();
+        $("#keyUrl").attr("href", KeyURL).html(KeyURL);
+        if (KeyIV != "") {
+            $("#KeyIV").html("IV: " + KeyIV);
+        } else {
+            $("#KeyIV").hide();
+        }
+    }
+    $('#loading').hide();
 }
-
-//链接列表
-function show_list(format) {
-	if (format === undefined) {
-		format = "";
-	}
-	var count = 0;
-	var KeyURL = "";
-	$("#media_file").val("");
-	for (let link of html) {
-		//密钥
-		if (link.indexOf("URI=") != -1) {
-			var re = /URI="(.*)"/.exec(link);
-
-			KeyURL = re[1];
-			if (isRelative(KeyURL)) {
-				if (KeyURL[0] == "/") {
-					KeyURL = RootPath + KeyURL;
-				} else {
-					KeyURL = BasePath + KeyURL;
-				}
-			}
-		}
-
-		//ts文件
-		if (link.indexOf("#") == -1 && link !== "" && link !== " " && link !== "\n" && link !== "\r") {
-			if (isRelative(link)) {
-				if (link[0] == "/") {
-					link = RootPath + link;
-				} else {
-					link = BasePath + link;
-				}
-			}
-			count++;
-			//判断是否m3u8
-			if (link.indexOf(".m3u8") != -1) {
-				$("#m3u8").hide();
-				$("button").hide();
-				$("#more_m3u8").show();
-				add_next_m3u8(link);
-				continue;
-			}
-			link = link.replace("\n", '');
-			link = link.replace("\r", '');
-			if (format != "") {
-				link = format.replace("$url$", link);
-			}
-			add_textarea(link);
-		}
-	}
-	$("#count").html("共" + count + "个文件");
-	if (KeyURL !== "") {
-		$(".keyUrl").show();
-		$("#keyUrl").attr("href", KeyURL).html(KeyURL);
-	}
-	$('#loading').hide();
-}
-show_list();
 
 //格式化
 $("#format").click(function () {
-	let formatStr = $("#formatStr").val();
-	show_list(formatStr);
+    let formatStr = $("#formatStr").val();
+    show_list(formatStr);
 });
-
 //下载 文本格式 按钮
 $("#DownText").click(function () {
-	var txt = $("#media_file").val();
-	txt = encodeURIComponent(txt);
-	chrome.downloads.download({
-		url: "data:text/plain," + txt
-	});
+    var txt = $("#media_file").val();
+    txt = encodeURIComponent(txt);
+    chrome.downloads.download({
+        url: "data:text/plain," + txt
+    });
 });
-$("#Downm3u8").click(function () {
-	chrome.downloads.download({ url: url });
-});
-
 //UrlDecode编码 按钮
 $("#UrlDecode").click(function () {
-	BasePath = getManifestUrlBase(false);
-	show_list();
+    BasePath = getManifestUrlBase(false);
+    show_list();
 });
-
 //刷新还原 按钮
 $("#Refresh").click(function () {
-	BasePath = getManifestUrlBase(true);
-	$("#formatStr").val('wget "$url$"');
-	show_list();
+    BasePath = getManifestUrlBase();
+    $("#formatStr").val('wget "$url$"');
+    show_list();
+});
+//把远程文件替换成本地文件
+$("#DownFixm3u8").click(function () {
+    $("#media_file").val("");
+    let textarea = "";
+    let m3u8_split = m3u8_content.split("\n");
+    for (let line of m3u8_split) {
+        if (line.includes("URI=")) {
+            let KeyURL = /URI="(.*)"/.exec(line)[1];
+            KeyURL = GetFile(KeyURL);
+            line = line.replace(/URI="(.*)"/, 'URI="' + KeyURL + '"');
+        }
+        if (!line.includes("#")) {
+            line = GetFile(line);
+        }
+        textarea = textarea + line + "\n";
+    }
+    $("#media_file").val(textarea);
 });
