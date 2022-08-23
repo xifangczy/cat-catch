f38e3d58481e2d5e07adcf817107372bf0ab089e
O2bmm <xifangczy@gmail.com>
2022-08-24 00:24:29 +0800

    Update mpd.js

--- js/mpd.js
+++ js/mpd.js
@@ -6,8 +6,15 @@ const _title = params.get("title");
 
 var mpdJson = {}; // 解析器json结果
 var mpdContent; // mpd文件内容
-var m3u8Content = "";
-var currentTabId;
+var m3u8Content = "";   //m3u8内容
+var mediaInfo = "" // 媒体文件信息
+var currentTabId = -1;  //当前tabId
+
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+    if (message == "getM3u8") {
+        sendResponse({ m3u8Content, mediaInfo });
+    }
+});
 
 $(function () {
     // 如果存在Referer修改当前标签下的所有xhr的Referer
@@ -57,10 +64,13 @@ $(function () {
     $("#videoToM3u8, #audioToM3u8").click(function () {
         let index = $("#mpdLists").val();
         let items = mpdJson.playlists[index];
+        let type = "video";
         if (this.id == "audioToM3u8") {
             index = $("#mpdAudioLists").val();
             items = mpdJson.mediaGroups.AUDIO.audio[index].playlists[0];
+            type = "audio";
         }
+        mediaInfo = getInfo(type);
         m3u8Content = "#EXTM3U\n";
         m3u8Content += "#EXT-X-VERSION:3\n";
         m3u8Content += "#EXT-X-TARGETDURATION:" + items.targetDuration + "\n";
@@ -73,30 +83,24 @@ $(function () {
         }
         m3u8Content += "#EXT-X-ENDLIST";
         // $("#media_file").html(m3u8Content); return;
-        chrome.tabs.create({ url: "m3u8.html?getId=" + currentTabId }, function (tab) {
-            chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-                if (message == "getM3u8") {
-                    sendResponse(m3u8Content);
-                }
-            });
-        });
+        chrome.tabs.create({ url: "m3u8.html?getId=" + currentTabId });
     });
 });
 
 function parseMPD() {
     $("#loading").hide(); $("#main").show();
     mpdJson = mpdParser.parse(mpdContent, { manifestUri: _url });
-    // console.log(mpdJson);
     for (let key in mpdJson.playlists) {
-        $("#mpdLists").append(`<option value='${key}'>
-            ${(mpdJson.playlists[key].attributes.BANDWIDTH / 1024).toFixed(1)} kbps | 
-            ${mpdJson.playlists[key].attributes["FRAME-RATE"].toFixed(1)} fps | 
-            ${mpdJson.playlists[key].attributes.RESOLUTION.width} x ${mpdJson.playlists[key].attributes.RESOLUTION.height}
-        </option>`);
+        $("#mpdLists").append(`<option value='${key}'>${(mpdJson.playlists[key].attributes.BANDWIDTH / 1024).toFixed(1)
+            } kbps |  ${mpdJson.playlists[key].attributes["FRAME-RATE"].toFixed(1)
+            } fps |  ${mpdJson.playlists[key].attributes.RESOLUTION.width
+            } x ${mpdJson.playlists[key].attributes.RESOLUTION.height
+            }</option>`);
     }
     for (let key in mpdJson.mediaGroups.AUDIO.audio) {
         $("#mpdAudioLists").append(`<option value='${key}'>${key}</option>`);
     }
+    $("#info").html(getInfo("video"));
     showSegment("video", 0);
 }
 
@@ -109,4 +113,13 @@ function showSegment(type, index) {
     $("#media_file").html(textarea);
     $("#count").html("共 " + items.segments.length + " 个文件" + "，总时长: " + secToTime(mpdJson.duration));
     $("#tips").html('initialization: <input type="text" value="' + items.segments[0].map.resolvedUri + '" spellcheck="false" readonly="readonly">');
+    $("#info").html(getInfo(type));
+}
+
+function getInfo(type = "audio") {
+    if (type == "audio") {
+        return "音频: " + $("#mpdAudioLists").find("option:selected").text();
+    } else {
+        return "视频: " + $("#mpdLists").find("option:selected").text();
+    }
 }
