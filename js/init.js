c2edfdc19e5e57f749820c7cb9cab7e8defe9c02
O2bmm <xifangczy@gmail.com>
2022-10-25 20:12:59 +0800

    📦refactor: 默认开启网站图标

--- js/init.js
+++ js/init.js
@@ -130,7 +130,7 @@ function GetDefault(Obj) {
         case "OtherAutoClear": return 100;
         case "Player": return "";
         case "Regex": return defaultRegex;
-        case "ShowWebIco": return false;
+        case "ShowWebIco": return true;
         case "MobileUserAgent":
             return "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1";
         case "m3u8dl": return false;
