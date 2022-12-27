cf94ab2294abc98e48055ec581b870d50b14e699
O2bmm <xifangczy@gmail.com>
2022-12-27 22:32:12 +0800

    Update init.js

--- js/init.js
+++ js/init.js
@@ -77,7 +77,6 @@ const reFilename = /filename="?(.*)"?/;
 const reRange = /([\d]+)-([\d]+)\/([\d]+)/;
 const reYoutube = /&range=[^&]*|&rbuf=[^&]*|&rn=[^&]*|&cver=[^&]*|&altitags=[^&]*|&pot=[^&]*|&fallback_count=[^&]*/g;
 const reStringModify = /['\\:\*\?"<\/>\|~]/g;
-// const reOptionsType = /^[^\/]+\/[^\/]+$/ig;
 
 // 防抖
 let debounce = undefined;
