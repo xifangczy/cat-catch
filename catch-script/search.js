6f57afaff1992907329fa1ff8abe7e81432a59ed
O2bmm <xifangczy@gmail.com>
2023-01-16 21:25:45 +0800

    Update search.js

--- catch-script/search.js
+++ catch-script/search.js
@@ -44,12 +44,9 @@ async function findMedia(data, raw = undefined, depth = 0) {
                 isFullM3u8(data[key]) && toUrl(data[key]);
                 continue;
             }
-            if (data[key].substring(0, 34).toLowerCase() == "data:application/vnd.apple.mpegurl") {
-                let text = data[key].substring(35);
-                if (text.substring(0, 7).toLowerCase() == "base64,") {
-                    text = window.atob(text.substring(7));
-                }
-                toUrl(text);
+            if (data[key].substring(0, 17).toLowerCase() == "data:application/") {
+                const text = getDataM3U8(data[key].substring(17));
+                text && toUrl(text);
                 continue;
             }
             if (data[key].toLowerCase().includes("urn:mpeg:dash:schema:mpd")) {
@@ -76,24 +73,18 @@ XMLHttpRequest.prototype.open = function (method) {
             window.postMessage({ action: "catCatchAddKey", key: this.response, href: location.href, ext: "key" });
         }
         if (this.response == "" || typeof this.response != "string") { return; }
-        if (this.response.substring(0, 34).toLowerCase() == "data:application/vnd.apple.mpegurl") {
-            let text = this.response.substring(35);
-            if (text.substring(0, 7).toLowerCase() == "base64,") {
-                text = window.atob(text.substring(7));
-            }
-            toUrl(text);
+        if(this.response.substring(0, 17).toLowerCase() == "data:application/"){
+            const text = this.response.substring(17);
+            toUrl(getDataM3U8(text));
             return;
         }
-        if (this.responseURL.substring(0, 34).toLowerCase() == "data:application/vnd.apple.mpegurl") {
-            let text = this.responseURL.substring(35);
-            if (text.substring(0, 7).toLowerCase() == "base64,") {
-                text = window.atob(text.substring(7));
-            }
-            toUrl(text);
+        if(this.responseURL.substring(0, 17).toLowerCase() == "data:application/"){
+            const text = getDataM3U8(this.responseURL.substring(17));
+            text && toUrl(text);
             return;
         }
         if (isUrl(this.response)) {
-            let ext = getExtension(this.response);
+            const ext = getExtension(this.response);
             ext && window.postMessage({ action: "catCatchAddMedia", url: this.response, href: location.href, ext: ext });
             return;
         }
@@ -157,12 +148,9 @@ window.fetch = async function (input, init) {
                 isFullM3u8(text) && toUrl(text);
                 return;
             }
-            if (text.substring(0, 34).toLowerCase() == "data:application/vnd.apple.mpegurl") {
-                let text = text.substring(35);
-                if (text.substring(0, 7).toLowerCase() == "base64,") {
-                    text = window.atob(text.substring(7));
-                }
-                toUrl(text);
+            if (text.substring(0, 17).toLowerCase() == "data:application/") {
+                const text = getDataM3U8(text.substring(0, 17));
+                text && toUrl(text);
                 return;
             }
         });
@@ -265,4 +253,20 @@ function getExtension(str) {
 function toUrl(text, ext = "m3u8") {
     let url = URL.createObjectURL(new Blob([new TextEncoder("utf-8").encode(text)]));
     window.postMessage({ action: "catCatchAddMedia", url: url, href: location.href, ext: ext });
+}
+function getDataM3U8(text){
+    const type = ["vnd.apple.mpegurl", "x-mpegurl"];
+    let isM3U8 = false;
+    for (let item of type){
+        if(text.substring(0, item.length).toLowerCase() == item){
+            text = text.substring(item.length + 1);
+            isM3U8 = true;
+            break;
+        }
+    }
+    if(!isM3U8){ return false; }
+    if (text.substring(0, 7).toLowerCase() == "base64,") {
+        return window.atob(text.substring(7));
+    }
+    return text;
 }
