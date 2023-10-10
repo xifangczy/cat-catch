754e2599b1513a295d9c00f2423244601b66aefd
O2bmm <xifangczy@gmail.com>
2023-10-10 12:18:14 +0800

    📦refactor: firefox declarativeNetRequest

--- js/firefox.js
+++ js/firefox.js
@@ -13,45 +13,45 @@ if (typeof (browser) == "object") {
     }
 
     // webRequest.onBeforeSendHeaders To declarativeNetRequest.updateSessionRules
-    var webRequestData = {};
-    function userAgentListener(details) {
-        for (var i = 0; i < details.requestHeaders.length; ++i) {
-            if (details.requestHeaders[i].name === webRequestData.addRules[0].action.requestHeaders[0].header) {
-                details.requestHeaders[i].value = webRequestData.addRules[0].action.requestHeaders[0].value;
-                return { requestHeaders: details.requestHeaders };
-            }
-        }
-        details.requestHeaders.push({
-            name: webRequestData.addRules[0].action.requestHeaders[0].header,
-            value: webRequestData.addRules[0].action.requestHeaders[0].value
-        });
-        return { requestHeaders: details.requestHeaders };
-    }
-    chrome.declarativeNetRequest = new Object();
-    chrome.declarativeNetRequest.updateSessionRules = (obj, callback) => {
-        webRequestData = obj;
-        if (obj.addRules == undefined) {
-            browser.webRequest.onBeforeSendHeaders.removeListener(userAgentListener);
-            return;
-        }
-        browser.webRequest.onBeforeSendHeaders.addListener(
-            userAgentListener, { urls: ["<all_urls>"], tabId: obj.addRules[0].id }, ["blocking", "requestHeaders"]
-        );
-        callback && callback();
-    };
-    chrome.declarativeNetRequest.getSessionRules = () => {
-        chrome.tabs.query({}, function (tabs) {
-            let allTabId = [];
-            for (let item of tabs) {
-                allTabId.push(item.id);
-            }
-            for (let item of G.featMobileTabId) {
-                if (!allTabId.includes(item)) {
-                    mobileUserAgent(item, false);
-                }
-            }
-        });
-    }
+    // var webRequestData = {};
+    // function userAgentListener(details) {
+    //     for (var i = 0; i < details.requestHeaders.length; ++i) {
+    //         if (details.requestHeaders[i].name === webRequestData.addRules[0].action.requestHeaders[0].header) {
+    //             details.requestHeaders[i].value = webRequestData.addRules[0].action.requestHeaders[0].value;
+    //             return { requestHeaders: details.requestHeaders };
+    //         }
+    //     }
+    //     details.requestHeaders.push({
+    //         name: webRequestData.addRules[0].action.requestHeaders[0].header,
+    //         value: webRequestData.addRules[0].action.requestHeaders[0].value
+    //     });
+    //     return { requestHeaders: details.requestHeaders };
+    // }
+    // chrome.declarativeNetRequest = new Object();
+    // chrome.declarativeNetRequest.updateSessionRules = (obj, callback) => {
+    //     webRequestData = obj;
+    //     if (obj.addRules == undefined) {
+    //         browser.webRequest.onBeforeSendHeaders.removeListener(userAgentListener);
+    //         return;
+    //     }
+    //     browser.webRequest.onBeforeSendHeaders.addListener(
+    //         userAgentListener, { urls: ["<all_urls>"], tabId: obj.addRules[0].id ?? -1 }, ["blocking", "requestHeaders"]
+    //     );
+    //     callback && callback();
+    // };
+    // chrome.declarativeNetRequest.getSessionRules = () => {
+    //     chrome.tabs.query({}, function (tabs) {
+    //         let allTabId = [];
+    //         for (let item of tabs) {
+    //             allTabId.push(item.id);
+    //         }
+    //         for (let item of G.featMobileTabId) {
+    //             if (!allTabId.includes(item)) {
+    //                 mobileUserAgent(item, false);
+    //             }
+    //         }
+    //     });
+    // }
 
     // Firefox scripting API 不完善
     chrome.scripting = new Object();
