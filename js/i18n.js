b7c55d186154ef2aac24a5cd44788d7c2805ac3f
O2bmm <xifangczy@gmail.com>
2024-01-15 03:11:38 +0800

    ☀: i18n

--- js/i18n.js
+++ js/i18n.js
@@ -1,7 +1,9 @@
-document.querySelectorAll('[data-i18n], [data-i18n-outer]').forEach(function (element) {
-    if (element.dataset.i18n) {
-        element.innerHTML = i18n(element.dataset.i18n);
-        return;
-    }
+document.querySelectorAll('[data-i18n]').forEach(function (element) {
+    element.innerHTML = i18n(element.dataset.i18n);
+});
+document.querySelectorAll('[data-i18n-outer]').forEach(function (element) {
     element.outerHTML = i18n(element.dataset.i18nOuter);
+});
+document.querySelectorAll('[data-i18n-placeholder]').forEach(function (element) {
+    element.setAttribute('placeholder', i18n(element.dataset.i18nPlaceholder));
 });
