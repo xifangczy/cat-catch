17aefaddc422462f8ec2888de9110587e38fec96
O2bmm <xifangczy@gmail.com>
2024-11-11 15:43:59 +0800

    Update downloader.js

--- js/downloader.js
+++ js/downloader.js
@@ -1,12 +1,12 @@
 // url 参数解析
 const params = new URL(location.href).searchParams;
-const _requestId = params.get("requestId") ? params.get("requestId").split(",") : [];
-const _ffmpeg = params.get("ffmpeg");
-let _downStream = params.get("downStream");
-const _data = [];
-const _taskId = Date.parse(new Date());
-let _tabId = null;
-let _index = null;
+const _requestId = params.get("requestId") ? params.get("requestId").split(",") : [];   // 要下载得资源ID
+const _ffmpeg = params.get("ffmpeg");   // 启用在线FFmpeg
+let _downStream = params.get("downStream"); // 启用边下边存 流式下载
+const _data = [];   // 通过_requestId获取得到得数据
+const _taskId = Date.parse(new Date()); // 配合ffmpeg使用的任务ID 以便在线ffmpeg通过ID知道文件属于哪些任务
+let _tabId = null;  // 当前页面tab id
+let _index = null;  // 当前页面 tab index
 
 // 是否表单提交下载 表单提交 不使用自定义文件名
 let _formDownload = false;
@@ -26,7 +26,7 @@ chrome.tabs.getCurrent(function (tabs) {
                 requestId: 1,
             }];
             _downStream = $("#downStream").prop("checked");
-            _formDownload = true;
+            _formDownload = true;   // 标记为表单提交下载
             _data.push(...data);
             setHeaders(data, () => { awaitG(start); });
             $("#getURL, .newDownload").toggle();
@@ -35,7 +35,7 @@ chrome.tabs.getCurrent(function (tabs) {
     }
     // 读取要下载的资源数据
     chrome.runtime.sendMessage({ Message: "getData", requestId: _requestId }, function (data) {
-        if (data == "error") {
+        if (data == "error" || !Array.isArray(data) || chrome.runtime.lastError) {
             chrome.tabs.highlight({ tabs: _index });
             alert(i18n.dataFetchFailed);
             return;
@@ -52,8 +52,9 @@ function start() {
 
     const $downBox = $("#downBox"); // 下载列表容器
     const down = new Downloader(_data);  // 创建下载器 
-    const itemDOM = new Map();  // 下载列表dom对象
+    const itemDOM = new Map();  // 提前储存需要平凡操作的dom对象 提高效率
 
+    // 添加html
     const addHtml = (fragment) => {
         const html = $(`
             <div class="downItem">
@@ -104,6 +105,7 @@ function start() {
             fragment.title = stringModify(fragment.title);    // 防止标题中有路径分隔符 导致下载新建文件夹
             fragment._filename = templates(G.downFileName, fragment);
         }
+        _formDownload = false;   // 重置表单提交标记
 
         // 流式下载处理
         if ((_downStream || G.downStream || fragment?._size > 2147483648) && !_ffmpeg) {
@@ -111,9 +113,10 @@ function start() {
         }
     }
 
-    // 下载列表处理
+    // 下载列表添加对应html
     down.fragments.forEach((fragment) => addHtml(fragment));
 
+    // 文件进程事件
     let lastEmitted = Date.now();
     down.on('itemProgress', function (fragment, state, receivedLength, contentLength, value) {
         const $dom = itemDOM.get(fragment.index);
@@ -129,6 +132,7 @@ function start() {
             fragment.fileStream.write(new Uint8Array(value));
         }
 
+        // 通过 lastEmitted 限制更新频率 避免疯狂dom操作
         if (Date.now() - lastEmitted >= 100) {
             if (contentLength) {
                 const progress = (receivedLength / contentLength * 100).toFixed(2) + "%";
@@ -147,16 +151,20 @@ function start() {
             lastEmitted = Date.now();
         }
     });
+
+    // 单文件下载完成事件
     down.on('completed', function (buffer, fragment) {
-        // 流式下载
+        // 是流式下载 停止写入
         if (fragment.fileStream) {
             fragment.fileStream.close();
             fragment.fileStream = null;
             return;
         }
 
+        // 更新标题
         document.title = `${down.success}/${down.total}`;
 
+        // 转为blob
         const blob = new Blob([buffer], { type: fragment.contentType });
         const blobUrl = URL.createObjectURL(blob);
 
@@ -175,12 +183,15 @@ function start() {
             fragment.downId = downloadId;
         });
     });
+
+    // 全部下载完成事件
     down.on('allCompleted', function (buffer) {
         $("#stopDownload").hide();
     });
 
     // 错误处理
     down.on('downloadError', function (fragment, error) {
+        // 添加range请求头 重新尝试下载
         if (!fragment.retry) {
             fragment.retry = 'range';
             down.downloader(fragment);
@@ -190,15 +201,18 @@ function start() {
         chrome.tabs.highlight({ tabs: _index });
     });
 
+    // 开始下载事件 如果存在range重下标记 则添加 range 请求头
     down.on('start', function (fragment, options) {
         if (fragment.retry && fragment.retry == 'range') {
             options.headers = { "Range": "bytes=0-" };
             options.cache = "no-cache";
         }
     });
-    // 全部停止下载
+
+    // 全部停止下载按钮
     $("#stopDownload").click(function () {
         down.stop();
+        // 更新对应的按钮状态
         itemDOM.forEach((item) => {
             if (item.button.data("action") == "stop") {
                 item.button.html(i18n.retryDownload).data("action", "start");
@@ -207,7 +221,7 @@ function start() {
         });
     });
 
-    // 打开目录
+    // 打开下载目录
     $(".openDir").click(function () {
         if (down.fragments[0].downId) {
             chrome.downloads.show(down.fragments[0].downId);
@@ -216,14 +230,30 @@ function start() {
         chrome.downloads.showDefaultFolder();
     });
 
+    // 监听事件
     chrome.runtime.onMessage.addListener(function (Message, sender, sendResponse) {
-        if (!Message.Message || Message.Message != "catCatchFFmpegResult" || Message.state != "ok" || _tabId == 0 || Message.tabId != _tabId || down.success != down.total) { return; }
+        if (!Message.Message) { return; }
+        // 添加下载任务
+        if (Message.Message == "catDownload" && Message.data && Array.isArray(Message.data) && !_ffmpeg) {
+            setHeaders(Message.data, () => {
+                Message.data.forEach((fragment) => {
+                    _data.push(fragment);
+                    down.push(fragment);
+                    addHtml(fragment);
+                    down.downloader(fragment.index);
+                });
+            });
+            sendResponse("OK");
+            return;
+        }
+        // 在线ffmpeg返回结果 关闭窗口
+        if (Message.Message != "catCatchFFmpegResult" || Message.state != "ok" || _tabId == 0 || Message.tabId != _tabId || down.success != down.total) { return; }
         setTimeout(() => {
             $("#autoClose").prop("checked") && window.close();
         }, Math.ceil(Math.random() * 999));
     });
 
-    // 监听下载
+    // 监听下载事件 下载完成 关闭窗口
     chrome.downloads.onChanged.addListener(function (downloadDelta) {
         if (!downloadDelta.state || downloadDelta.state.current != "complete") { return; }
         if (!down.fragments.some(item => item.downId == downloadDelta.id)) { return }
@@ -237,23 +267,7 @@ function start() {
         }
     });
 
-    // 监听添加任务
-    chrome.runtime.onMessage.addListener(function (Message, sender, sendResponse) {
-        if (Message.Message == "catDownload" && !_ffmpeg) {
-            setHeaders(Message.data, () => {
-                Message.data.forEach((fragment) => {
-                    down.push(fragment);
-                    addHtml(fragment);
-                    down.downloader(fragment.index);
-                });
-            });
-            sendResponse("OK");
-            return;
-        }
-    });
-
     document.title = `${down.success}/${down.total}`;
-
     down.start();
 }
 
