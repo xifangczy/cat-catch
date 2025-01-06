bbd7563ddb16dc46e5d0eeee8e0b8ddc992eac6b
O2bmm <xifangczy@gmail.com>
2025-01-06 17:46:32 +0800

    自动下载 自定义文件名

--- js/background.js
+++ js/background.js
@@ -215,11 +215,25 @@ function findMedia(data, isRegex = false, filter = false, timer = false) {
         // 发送到popup 并检查自动下载
         chrome.runtime.sendMessage({ Message: "popupAddData", data: info }, function () {
             if (G.featAutoDownTabId.size > 0 && G.featAutoDownTabId.has(info.tabId)) {
-                const downDir = info.title == "NULL" ? "CatCatch/" : stringModify(info.title) + "/";
-                chrome.downloads.download({
-                    url: info.url,
-                    filename: downDir + info.name
-                });
+                // const downDir = info.title == "NULL" ? "CatCatch/" : stringModify(info.title) + "/";
+                // chrome.downloads.download({
+                //     url: info.url,
+                //     filename: downDir + info.name
+                // });
+
+                try {
+                    const downDir = info.title == "NULL" ? "CatCatch/" : stringModify(info.title) + "/";
+                    let fileName = isEmpty(info.name) ? stringModify(info.title) + '.' + info.ext : decodeURIComponent(stringModify(info.name));
+                    if (G.TitleName) {
+                        fileName = filterFileName(templates(G.downFileName, info));
+                    } else {
+                        fileName = downDir + fileName;
+                    }
+                    chrome.downloads.download({
+                        url: info.url,
+                        filename: fileName
+                    });
+                } catch (e) { return; }
             }
             if (chrome.runtime.lastError) { return; }
         });
