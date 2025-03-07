b8f764473ab5e2af6cdda7a3251e01a26c756230
O2bmm <xifangczy@gmail.com>
2025-03-08 01:22:13 +0800

    Update i18n.js

--- js/i18n.js
+++ js/i18n.js
@@ -1,19 +1,15 @@
 (function () {
     document.querySelectorAll('[data-i18n]').forEach(function (element) {
-        element.innerHTML = i18n(element.dataset.i18n);
+        element.innerHTML = i18n(element.dataset.i18n) ?? element.dataset.i18n;
     });
     document.querySelectorAll('[data-i18n-outer]').forEach(function (element) {
-        element.outerHTML = i18n(element.dataset.i18nOuter);
+        element.outerHTML = i18n(element.dataset.i18nOuter) ?? element.dataset.i18nOuter;
     });
     document.querySelectorAll('i18n').forEach(function (element) {
-        element.outerHTML = i18n(element.innerHTML);
+        element.outerHTML = i18n(element.innerHTML) ?? element.innerHTML;
     });
     document.querySelectorAll('[data-i18n-placeholder]').forEach(function (element) {
-        element.setAttribute('placeholder', i18n(element.dataset.i18nPlaceholder));
+        element.setAttribute('placeholder', i18n(element.dataset.i18nPlaceholder) ?? element.dataset.i18nPlaceholder);
     });
-
-    const title = i18n(document.title);
-    if (title) {
-        document.title = title;
-    }
+    document.title = i18n(document.title) ?? document.title;
 })();
