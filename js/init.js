9e5b2bedb91cc827fff42c32d09c5acaa25f2b7d
O2bmm <xifangczy@gmail.com>
2025-10-24 03:01:04 +0800

    Update init.js

--- js/init.js
+++ js/init.js
@@ -51,7 +51,7 @@ G.requestHeaders = new Map();   // 临时储存请求头
 G.urlMap = new Map();   // url查重map
 G.deepSearchTemporarilyClose = null; // 深度搜索临时变量
 
-G.damnUrl = [/^https:\/\/www\.douyin\.com\/.*$/i];
+G.damnUrl = [/^https:\/\/.*\.douyin\.com\/.*$/i];
 G.damnUrlSet = new Set();
 
 // 初始化当前tabId
