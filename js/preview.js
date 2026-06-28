bdbdf50d0b4085eaebaf829ef60ac413d7ca698f
O2bmm <xifangczy@gmail.com>
2026-06-28 15:43:31 +0800

    Update preview.js

--- js/preview.js
+++ js/preview.js
@@ -268,15 +268,13 @@ class FilePreview {
      */
     deleteItem(data = null) {
         data = data ? [data] : this.getSelectedItems();
-        data.forEach(item => {
-            let index = -1
-            do {
-                index = this.originalItems.findIndex(originalItem => originalItem.requestId === item.requestId || (this.deleteDuplicateFilenames && originalItem.name === item.name));
-                if (index !== -1) {
-                    this.originalItems.splice(index, 1);
-                }
-            } while (index !== -1);
+
+        const deleteIds = new Set(data.map(item => item.requestId));
+        const deleteNames = this.deleteDuplicateFilenames ? new Set(data.map(item => item.name)) : new Set();
+        this.originalItems = this.originalItems.filter(item => {
+            return !deleteIds.has(item.requestId) && !(this.deleteDuplicateFilenames && deleteNames.has(item.name));
         });
+
         this.updateFileList();
     }
     /**
